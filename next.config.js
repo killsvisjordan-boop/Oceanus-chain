297fe06b5c60b5ec96336c2a8ca413db6e23e8ac
killsvisjordan-boop <killsvisjordan@gmail.com>
2026-06-17 14:10:59 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-        permanent: false,
-      },
-    ]
-  },
-}
+const nextConfig = {}
 module.exports = nextConfig
